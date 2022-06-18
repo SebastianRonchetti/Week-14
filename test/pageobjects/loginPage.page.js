76d0d2d3eae0af8656ee0edf706212e845a302ac
Sebastián Ronchetti <ronchettisebastian@gmail.com>
2022-06-18 20:05:18 -0300

    BaSP-W14-2022-QA: First push

---
+++ test/pageobjects/loginPage.page.js
@@ -0,0 +1,11 @@
+class loginPage {
+    //getters
+    get userNameInput() {return $(`#user-name`)};
+    get passwordInput() {return $(`#password`)};
+    get loginButton() {return $(`#login-button`)};
+    //setters
+
+    //methods
+}
+
+module.exports = new loginPage();
